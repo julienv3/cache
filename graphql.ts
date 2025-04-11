062ef726fd19f8a0de39060e6badf7ebe27ea057
Julien Vermette <juverm@chainguard.dev>
2025-04-11 11:16:10 -0400

    Vercel Support recommended header

--- graphql.ts
+++ graphql.ts
@@ -10,10 +10,19 @@ export default async function handler(
   console.info(`Serverless function ${request.url} was invoked`);
 
   const crash = request.url.includes("crash");
-  response.statusCode = crash ? 500 : 200;
   response.setHeader("x-serverless-function", request.url as string);
   response.setHeader("content-type", "application/json");
-  return response.json({
-    message: crash ? "I crashed :(" : `Hello World from ${request.url}`,
-  });
+
+  if (crash) {
+    response.statusCode = 500;
+    response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+    return response.json({
+      message: "I crashed :(",
+    });
+  } else {
+    response.statusCode = 200;
+    return response.json({
+      message: `Hello World from ${request.url}`,
+    });
+  }
 }
